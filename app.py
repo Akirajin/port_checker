6911c5117ea23386742c4267934e3877e0f1655b
Akirajin <tueda@uolinc.com>
2020-06-29 17:08:44 -0300

    ajustes na porta para 80

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 @app.route("/check/ip/<string:ip>/port/<string:port>")
 def hello_world(ip, port):
     res = has_open_port(ip, port)
-    return f'{res}'
+    return str(res)
 
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=80)
