006d541b59e31e221730ead00b64eaf1ccfa09d5
Akirajin <Akirajin@users.noreply.github.com>
2020-07-30 18:24:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,4 +13,5 @@ def hello_world(ip, port):
 
 if __name__ == '__main__':
 #    app.run(host='0.0.0.0', port=8080)
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
